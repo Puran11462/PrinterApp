1fb4a6758696e459d37febaeeec10368ea537f1c
Your Name <xettripuran81@gmail.com>
2024-10-18 17:24:08 +0300

    changing the value of number1

--- PrinterApp.java
+++ PrinterApp.java
@@ -8,9 +8,11 @@ public class PrinterApp {
         System.out.println("This app is made by:");
         String creator="Puran Karki";
         System.out.println("This app is made by: "+creator);
-        int number1=8;
+        int number1=18;
         int number2=13;
-        
+        //System.out.println(number1);
+        System.out.println("The value of number1 is: "+number1);
+
 
     }
 }
