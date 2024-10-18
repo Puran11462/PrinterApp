512183c5ef35de3c51c2bc9456446edbf1682af5
Your Name <xettripuran81@gmail.com>
2024-10-18 17:11:40 +0300

    first commit

---
+++ PrinterApp.java
@@ -0,0 +1,7 @@
+public class PrinterApp {
+
+    public static void main(String[] args) {
+        System.out.println("Printer App");
+        System.out.println("This app is made by:");
+    }     
+}
