d703765a806137c02618e48088464b670b134bff
Your Name <xettripuran81@gmail.com>
2024-10-18 17:33:19 +0300

    declaring the interger and double type variables

--- PrinterApp.java
+++ PrinterApp.java
@@ -8,14 +8,19 @@ public class PrinterApp {
         System.out.println("This app is made by:");
         String creator="Puran Karki";
         System.out.println("This app is made by: "+creator);
-        int number1=18;
-        int number2=13;
+        int number1=2;
+        int number2=4;
         //System.out.println(number1);
         System.out.println("The value of number1 is: "+number1);
         System.out.println("The value of number2 is: "+number2);
         int sum=number1+number2;    
         int multiplication;
-
+        multiplication=number1*number2;
+        System.out.println(multiplication);
+        System.out.println(number1+" * "+number2+" = "+multiplication);
+        int aadition;
+        int subtraction;
+        double division;
 
     }
 }
