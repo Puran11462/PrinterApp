2b35373d03b840aa1580fffc6dd4b69b4d5b69fc
Your Name <xettripuran81@gmail.com>
2024-10-19 00:19:22 +0300

    declaring String type variable called creator and moving it to top

--- PrinterApp.java
+++ PrinterApp.java
@@ -4,13 +4,14 @@
 public class PrinterApp {
 
     public static void main(String[] args) {
+        String creator;
         int multiplication;
         int addition;
         int subtraction;
         double division;
         System.out.println("PrinterApp");
         System.out.println("This app is made by:");
-        String creator="Puran Karki";
+        creator="Puran Karki";
         System.out.println("This app is made by: "+creator);
         int number1=2;
         int number2=4;
