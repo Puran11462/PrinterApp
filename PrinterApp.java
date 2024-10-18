3f60c43efc44e4864a071e67310704db7bfb4375
Your Name <xettripuran81@gmail.com>
2024-10-18 17:20:44 +0300

    Assainging values to the variables

--- PrinterApp.java
+++ PrinterApp.java
@@ -1,7 +1,16 @@
+/**
+ * PrinterApp
+ */
 public class PrinterApp {
 
     public static void main(String[] args) {
-        System.out.println("Printer App");
+        System.out.println("PrinterApp");
         System.out.println("This app is made by:");
-    }     
+        String creator="Puran Karki";
+        System.out.println("This app is made by: "+creator);
+        int number1=8;
+        int number2=13;
+        
+
+    }
 }
